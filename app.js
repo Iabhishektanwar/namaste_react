cd0eb46716eab869733e0114b512748a4772c0d6
Iabhishektanwar <abhishektanwar999@gmail.com>
2023-09-19 16:52:02 +0530

    Food delivery app first commit

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
 /**
  *
- * ReactElement(Object) => HTML(Browser understandable)
+ * Header
+ *  - Logo
+ *  - Nav Items
+ * Body
+ *  - Search
+ *  - ResturantContainer
+ *    - Resturants
+ * Footer
+ *  - Links
+ *  - Address
+ *  - Contact
  *
  */
 
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img
+          className="logo"
+          src="https://img.freepik.com/premium-vector/burger-logo-template_441059-18.jpg"
+          alt="logo"
+        />
+      </div>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="Search">Search</div>
+      <div className="resturant-container"></div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+      <Body />
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-const parent = React.createElement("div", { id: "parent" }, [
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I am a h1 tag!!!"),
-    React.createElement("h2", {}, "I am a h2 tag!!!"),
-  ]),
-  React.createElement("div", { id: "child2" }, [
-    React.createElement("h1", {}, "I am a h1 tag!!!"),
-    React.createElement("h2", {}, "I am a h2 tag!!!"),
-  ]),
-  React.createElement("div", { id: "child3" }, [
-    React.createElement("h1", {}, "I am a h1 tag!!!"),
-    React.createElement("h2", {}, "I am a h2 tag!!!"),
-  ]),
-]);
-
-root.render(parent);
+root.render(<AppLayout />);
